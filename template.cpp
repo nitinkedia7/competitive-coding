c5cb0dd563290a3b8d71b4329400a0cf7e3e4c62
Nitin Kedia <nitinkedia7@gmail.com>
2019-12-14 16:33:52 +0530

    Fix few errors in template

--- template.cpp
+++ template.cpp
@@ -1,24 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define pb push_back
-#define popb pop_back
-#define vi vector<int>
-#define vll vector<ll>
-#define vvi vector < vi >
-#define pii pair<int,int>
-#define pll pair<long long, long long>
-#define all(c) c.begin(),c.end()
-#define mp make_pair
-#define f first
-#define s second
 typedef long long ll;
-typedef unsigned long long ull;
+const ll MOD = 1000000007;
 
-int main ()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+void sherlock(int t) {
+    
+}
 
-	return 0;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tt;
+    cin >> tt;
+    for (int t = 1; t <= tt; t++) sherlock(t);
+    return 0;
 }
